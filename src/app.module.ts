6412ef3a6c95b9844cf0dc976648f9fa86f9737b
padub <pierrealain.dubrulle@gmail.com>
2024-11-16 01:42:50 +0100

    creation of skill resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SkillModule } from './skill/skill.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [SkillModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
